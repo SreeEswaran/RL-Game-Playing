5cb6454c53281329c7b7f1f26b4c6d7f240fdf6e
Sree Deekshitha Yerra <126445402+SreeEswaran@users.noreply.github.com>
2024-06-15 00:05:15 +0530

    Create agent.py

---
+++ agent.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class QLearningAgent:
+    def __init__(self, state_size, action_size, alpha=0.1, gamma=0.99, epsilon=0.1):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.q_table = np.zeros((state_size, action_size))
+        
+    def choose_action(self, state):
+        if np.random.rand() <= self.epsilon:
+            return np.random.choice(self.action_size)
+        return np.argmax(self.q_table[state])
+    
+    def learn(self, state, action, reward, next_state):
+        best_next_action = np.argmax(self.q_table[next_state])
+        td_target = reward + self.gamma * self.q_table[next_state, best_next_action]
+        td_error = td_target - self.q_table[state, action]
+        self.q_table[state, action] += self.alpha * td_error
